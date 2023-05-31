9fa25ebe53715963c827c4dbccb28b15890592c6
Buddylasta <thecallousedcoin@protonmail.com>
2023-05-30 20:07:51 -0500

    remove caching

--- app/load_data.py
+++ app/load_data.py
@@ -3,20 +3,17 @@ import requests
 import pandas as pd
 import json
 
-@st.cache_data
 def get_user_data(url):
     response = requests.get(url)
     json_data = response.json()
     return json_data
 
 # Create dataframe from ckpool data
-@st.cache_data
 def load_pool_data(pool_data):
     df = pd.read_json(pool_data, orient='index')
     return df
 
 # Get ckpool data
-@st.cache_data
 def get_pool_data(url):
     response = requests.get(url)
 
